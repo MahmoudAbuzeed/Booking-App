83680706d2a015846bf2485c1969ca5351ee947c
Mahmoud Abuzeed <mahmoudabozeed56@gmail.com>
2020-03-23 22:14:43 +0200

    create booking model and complete mutation

--- app.js
+++ app.js
@@ -1,89 +1,32 @@
 require('dotenv').config();
 
+
 const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const garphqlHttp = require("express-graphql");
-const { buildSchema } = require("graphql");
+const cors = require('cors');
 
-const Event = require('./models/event');
+const graphQlSchema = require('./garphql/schema/index');
+const graphQlResolvers = require('./garphql/resolvers/index');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
+
+
 
 app.use(
   "/graphql",
   garphqlHttp({
-    schema: buildSchema(`
-    
-    type Event {
-        _id: ID!
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-    }
-
-
-    input EventInput {
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-    }
-        
-    type RootQuery {
-        events: [Event!]!
-    }
-
-    type RootMutation {
-         createEvent(eventInput: EventInput): Event
-    }
-
-
-    schema {
-        query: RootQuery
-        mutation: RootMutation
-    }
-    
-    `),
-    rootValue: {
-      events: () => {
-        return Event.find()
-        .then(events =>{
-           return events.map(event =>{
-            return { ...event._doc};
-            });
-        })
-        .catch(err =>{
-            throw err;
-        })
-      },
-      createEvent: args => {
-        const event = new Event({
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: new Date(args.eventInput.date)
-        });
-        return event
-          .save()
-          .then(result => {
-            console.log(result);
-            return { ...result._doc, _id: result._doc._id.toString() };
-          })
-          .catch(err => {
-            console.log(err);
-            throw err;
-          });
-      }
-    },
+    schema: graphQlSchema ,
+    rootValue: graphQlResolvers,
     graphiql: true
   })
 );
 
-const PORT = process.env.PORT || 3000 ; 
+const PORT = process.env.PORT || 5000 ; 
 
 app.listen(PORT, () =>{
 
